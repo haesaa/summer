24aa806b18d9d86be9dc16894f114095efd410b1
haesaa <88qwertyui8@gmail.com>
2026-02-04 17:56:42 +0900

    2026-02-04 수 요 일 version1

--- day0120/01try.py
+++
@@ -1,9 +0,0 @@
-# 01try.py
-num = 0 #변수 = variable = local 지역변수
-try :
-    num = int(input('숫자를 기술>>> '))
-except Exception as ex :
-    print('정지')
-
-print('결제금액 =', num*5, '원')
-  
--- day0120/game.py
+++
@@ -1,7 +0,0 @@
-# game.py
-
-import workGame as wg
-
-wg.zerg() #정상적인 call = 호출 workGame.zerg()
-wg.maple('winter', 5)
-
